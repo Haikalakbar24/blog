68bc68adca5f039f98344d94789a1d2e4601d84e
Haikalakbar24 <akbarkall24@gmail.com>
2021-12-31 01:28:17 +0700

    blog

--- application/controllers/Blog.php
+++ application/controllers/Blog.php
@@ -1,53 +1,71 @@
-	<?php
-	Class Blog extends CI_Controller
+<?php
+class Blog extends CI_Controller
+{
+    public function __Construct()
+    {
+        parent::__Construct();
+        $this->load->database();
+        $this->load->helper('url');
+        $this->load->model('Blog_model');
+    }
+
+    public function index()
+    {   
+     
+        $data['blog']=$this->Blog_model->getBlogs();
+        $this->load->view('blog',$data);
+
+    }
+    public function listdata()
+    {
+        $this->load->view('list_data');
+    }
+    public function detail($url)
+    {
+
+        $data['blog']=$this->Blog_model->getSingleBlog('url',$url);
+        $this->load->view('detail',$data);
+
+    }
+
+    public function add()
+    {
+        if($this->input->post())
+        {
+            $data['title']=$this->input->post('title');
+            $data['content']=$this->input->post('content');
+            $data['url']=$this->input->post('url');
+        $id=$this->Blog_model->insertBlog($data);
+        if($id)
+            echo "Data berhasil disimpan";
+        else
+            echo "Data gagal disimpan";
+        }
+        $this->load->view('form_add');
+    }
+
+    public function edit($id)
 	{
-	    public function index()
-	    {
-	       $this->load->database();
-		   $query=$this->db->get('blog');
-		   $data['blog']=$query->result_array();
-	        $this->load->view('blog',$data);
-	    }
-		public function detail($url)
-		{
-			$data['blog']=$this->Blog_model->getSingleBlog('url', $url);
-			$this->load->view('detail', $data);
+		$data['blog'] = $this->Blog_model->getSingleBlog('id',$id);
+		if ($this->input->post()) {
+			$post['title'] = $this->input->post('title');
+			$post['content'] = $this->input->post('content');
+			$post['url'] = $this->input->post('url');
+
+			$id = $this->Blog_model->updateBlog($id,$post);
+
+			if ($id)
+				echo "Data berhasil disimpan";
+			else
+				echo "Data gagal disimpan";
 		}
-		public function add()
-		{
-			if($this->input->post())
-			{
-				$data['title']=$this->input->post('title');
-				$data['content']=$this->input->post('content');
-				$data['url']=$this->input->post('url');
-				$id=$this->Blog_model->insertBlog($data);
-				if ($id)
-				echo "Data Berhasil Disimpan";
-				else
-				echo "Data Gagal Disimpan";
-			}
-			$this->load->view('form_add');
-		}
-		public function edit($id)
-		{
-			$data['blog']= $this->Blog_model->getSingleBlog('id',$id);
-			if($this->input->post())
-			{
-				$post['title']=$this->input->post('title');
-				$post['content']=$this->input->post('content');
-				$post['url']=$this->input->post('url');
-				$id =$this->Blog_model->updateBlog($id,$post);
-				if($id)
-				echo "Data Berhasil Disimpan";
-				else
-				echo "Data Gagal Disimpan";
-			}
-			$this->load->view('form_edit', $data);
-		}
-		public function deleteBlog($id)
-		{
-			$this->Blog_model->deleteBlog($id);
-			redirect('blog/index');
-		}
+		$this->load->view('form_edit',$data);
 	}
+
+    public function delete($id)
+    {
+        $this->Blog_model->deleteBlog($id);
+        redirect('Blog/index');
+    }
+}
 ?>
--- application/views/blog.php
+++ application/views/blog.php
@@ -1,22 +1,25 @@
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Document</title>
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
 <body>
-        
-        <h1>Artikel Terbaru</h1>
-        <a href ="<? echo site_url('blog/add');?>"> + Tambah Artikel
-        <?php foreach($blog as $key=>$blog):?>
-            <div class="blog"></div>
-                <h2>
-                    <a href="<?php echo ('blog/detail/' .$blog['url']);?>">
-                <?php echo $blog['title'];?>
-            </a>
+    <h1>Artikel Terbaru</h1>
+    <a href="<?php echo site_url('Blog/add') ?>"> Tambah Artikel</a>
+    <?php foreach($blog as $key=>$blog):?>
+    <div class="blog">
+        <h2>
+          <a href="<?php echo site_url('blog/detail/'.$blog['url']);?>">
+            <?php echo $blog['title'];?>
+          </a>
         </h2>
-            <a href="<?php echo site_url('blog/edit/' . $blog['id']); ?>">Edit</a>
+        <a href="<?php echo site_url('blog/edit/' . $blog['id']); ?>"> Edit</a>
+        <a href="<?php echo site_url('blog/delete/' . $blog['id']); ?>">Delete</a>
+        <br>
         <?php echo $blog['content'];
+        
         endforeach;
         ?>
-    </body>
+   
+</body>
 </html>
